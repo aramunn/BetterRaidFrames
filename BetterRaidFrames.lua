aramunn/BetterRaidFrames

commit a17f4a1b099b90ec7658d4ce3be098565c9bce30

Implement showing/hiding Shield/Absorb Bar
Implement showing/hiding Shield/Absorb Bar

BetterRaidFrames.lua
@@ -1420,9 +1420,28 @@ function BetterRaidFrames:ResizeBars(tRaidMember)
 	local wndMaxShield = tRaidMember.wndMaxShieldBar
 	local nLeft, nTop, nRight, nBottom = wndHealthBar:GetAnchorOffsets()
 	
-	wndHealthBar:SetAnchorOffsets(nLeft, nTop, nWidth * 0.67, nBottom)
-	wndMaxShield:SetAnchorOffsets(nWidth * 0.67, nTop, nWidth * 0.85, nBottom)
-	wndMaxAbsorb:SetAnchorOffsets(nWidth * 0.85, nTop, nWidth, nBottom)
+	-- Define offsets based on settings of which bars to show
+	wndMaxShield:Show(self.settings.bShowShieldBar)
+	wndMaxAbsorb:Show(self.settings.bShowAbsorbBar)
+	if self.settings.bShowShieldBar and self.settings.bShowAbsorbBar then
+		wndHealthBar:SetAnchorOffsets(nLeft, nTop, nWidth * 0.67, nBottom)
+		wndMaxShield:SetAnchorOffsets(nWidth * 0.67, nTop, nWidth * 0.85, nBottom)
+		wndMaxAbsorb:SetAnchorOffsets(nWidth * 0.85, nTop, nWidth, nBottom)
+	end
+	
+	if self.settings.bShowShieldBar and not self.settings.bShowAbsorbBar then
+		wndHealthBar:SetAnchorOffsets(nLeft, nTop, nWidth * 0.75, nBottom)
+		wndMaxShield:SetAnchorOffsets(nWidth * 0.75, nTop, nWidth, nBottom)
+	end
+	
+	if not self.settings.bShowShieldBar and self.settings.bShowAbsorbBar then
+		wndHealthBar:SetAnchorOffsets(nLeft, nTop, nWidth * 0.8, nBottom)
+		wndMaxAbsorb:SetAnchorOffsets(nWidth * 0.8, nTop, nWidth, nBottom)
+	end
+	
+	if not self.settings.bShowShieldBar and not self.settings.bShowAbsorbBar then
+		wndHealthBar:SetAnchorOffsets(nLeft, nTop, nWidth, nBottom)
+	end
 end
 
 function BetterRaidFrames:UpdateHPText(nHealthCurr, nHealthMax, tRaidMember, strCharacterName)

BetterRaidFrames.xml
@@ -209,11 +209,13 @@
                 <Event Name="ButtonCheck" Function="Button_TrackDebuffs"/>
                 <Event Name="ButtonUncheck" Function="Button_TrackDebuffs"/>
             </Control>
-            <Control Class="Button" Base="HologramSprites:HoloCheckBoxBtn" Font="DefaultButton" ButtonType="Check" RadioGroup="" LAnchorPoint="0" LAnchorOffset="3" TAnchorPoint="0" TAnchorOffset="60" RAnchorPoint="0" RAnchorOffset="260" BAnchorPoint="0" BAnchorOffset="86" DT_VCENTER="1" DT_CENTER="0" BGColor="UI_BtnBGDefault" TextColor="UI_BtnTextDefault" NormalTextColor="UI_BtnTextDefault" PressedTextColor="UI_BtnTextDefault" FlybyTextColor="UI_BtnTextDefault" PressedFlybyTextColor="UI_BtnTextDefault" DisabledTextColor="UI_BtnTextDefault" TooltipType="OnCursor" Name="Button_ShowShieldBar" TooltipColor="" Text="       Show Shield Bar (not working - placeholder)" TextId="" DT_SINGLELINE="1">
-                <Event Name="ButtonSignal" Function="YourFunctionName"/>
+            <Control Class="Button" Base="HologramSprites:HoloCheckBoxBtn" Font="DefaultButton" ButtonType="Check" RadioGroup="" LAnchorPoint="0" LAnchorOffset="3" TAnchorPoint="0" TAnchorOffset="60" RAnchorPoint="0" RAnchorOffset="260" BAnchorPoint="0" BAnchorOffset="86" DT_VCENTER="1" DT_CENTER="0" BGColor="UI_BtnBGDefault" TextColor="UI_BtnTextDefault" NormalTextColor="UI_BtnTextDefault" PressedTextColor="UI_BtnTextDefault" FlybyTextColor="UI_BtnTextDefault" PressedFlybyTextColor="UI_BtnTextDefault" DisabledTextColor="UI_BtnTextDefault" TooltipType="OnCursor" Name="Button_ShowShieldBar" TooltipColor="" Text="       Show Shield Bar" TextId="" DT_SINGLELINE="1">
+                <Event Name="ButtonCheck" Function="Button_ShowShieldBar"/>
+                <Event Name="ButtonUncheck" Function="Button_ShowShieldBar"/>
             </Control>
-            <Control Class="Button" Base="HologramSprites:HoloCheckBoxBtn" Font="DefaultButton" ButtonType="Check" RadioGroup="" LAnchorPoint="0" LAnchorOffset="3" TAnchorPoint="0" TAnchorOffset="85" RAnchorPoint="0" RAnchorOffset="260" BAnchorPoint="0" BAnchorOffset="111" DT_VCENTER="1" DT_CENTER="0" BGColor="UI_BtnBGDefault" TextColor="UI_BtnTextDefault" NormalTextColor="UI_BtnTextDefault" PressedTextColor="UI_BtnTextDefault" FlybyTextColor="UI_BtnTextDefault" PressedFlybyTextColor="UI_BtnTextDefault" DisabledTextColor="UI_BtnTextDefault" TooltipType="OnCursor" Name="Button_ShowAbsorbBar" TooltipColor="" Text="       Show Absorb Bar (not working - placeholder)" TextId="" DT_SINGLELINE="1">
-                <Event Name="ButtonSignal" Function="YourFunctionName"/>
+            <Control Class="Button" Base="HologramSprites:HoloCheckBoxBtn" Font="DefaultButton" ButtonType="Check" RadioGroup="" LAnchorPoint="0" LAnchorOffset="3" TAnchorPoint="0" TAnchorOffset="85" RAnchorPoint="0" RAnchorOffset="260" BAnchorPoint="0" BAnchorOffset="111" DT_VCENTER="1" DT_CENTER="0" BGColor="UI_BtnBGDefault" TextColor="UI_BtnTextDefault" NormalTextColor="UI_BtnTextDefault" PressedTextColor="UI_BtnTextDefault" FlybyTextColor="UI_BtnTextDefault" PressedFlybyTextColor="UI_BtnTextDefault" DisabledTextColor="UI_BtnTextDefault" TooltipType="OnCursor" Name="Button_ShowAbsorbBar" TooltipColor="" Text="       Show Absorb Bar" TextId="" DT_SINGLELINE="1">
+                <Event Name="ButtonCheck" Function="Button_ShowAbsorbBar"/>
+                <Event Name="ButtonUncheck" Function="Button_ShowAbsorbBar"/>
             </Control>
         </Control>
         <Control Class="Button" Base="CRB_UIKitSprites:btn_square_LARGE_Green" Font="Thick" ButtonType="PushButton" RadioGroup="" LAnchorPoint="0" LAnchorOffset="400" TAnchorPoint="0" TAnchorOffset="280" RAnchorPoint="0" RAnchorOffset="520" BAnchorPoint="0" BAnchorOffset="320" DT_VCENTER="1" DT_CENTER="1" BGColor="UI_BtnBGDefault" TextColor="UI_BtnTextDefault" NormalTextColor="UI_BtnTextDefault" PressedTextColor="UI_BtnTextDefault" FlybyTextColor="UI_BtnTextDefault" PressedFlybyTextColor="UI_BtnTextDefault" DisabledTextColor="UI_BtnTextDefault" TooltipType="OnCursor" Name="CloseButton" TooltipColor="" Text="" TextId="CRB_Close" DT_WORDBREAK="1" DT_SINGLELINE="1">

commit 00ef924d99b6f0765c5fb17ae56ee6baa35b3c49

Fixes bar art not updating properly when dead
Shield/absorb bar were still showing after dying. Caused by
a17f4a1b099b90ec7658d4ce3be098565c9bce30 - should be fixed now.
Also improved variable usage for showing mana bar.

BetterRaidFrames.lua
@@ -588,7 +588,11 @@ function BetterRaidFrames:UpdateAllMembers()
 		local wndMemberBtn = tRaidMember.wndRaidMemberBtn
 		local tMemberData = GroupLib.GetGroupMember(idx)
 		local unitMember = GroupLib.GetUnitForGroupMember(idx)
-
+		
+		-- Status variables
+		local bOutOfRange = tMemberData.nHealthMax == 0 or not unitMember
+		local bDead = tMemberData.nHealth == 0 and tMemberData.nHealthMax ~= 0
+		
 		-- Update bar art if dead -> no longer dead
 		self:UpdateBarArt(tMemberData, tRaidMember, unitMember)
 		
@@ -641,11 +645,11 @@ function BetterRaidFrames:UpdateAllMembers()
 				wndManaBar:SetMax(nManaMax)
 				wndManaBar:SetProgress(tMemberData.nMana)	
 			end
-			wndManaBar:Show(bShowManaBar and tMemberData.bIsOnline and not bDead and not bOutOfRange and unitMember and unitMember:GetHealth() > 0 and unitMember:GetMaxHealth() > 0)			
+			wndManaBar:Show(bShowManaBar and tMemberData.bIsOnline and not bDead and not bOutOfRange)			
 		end
 		
 		-- Scaling
-		self:ResizeBars(tRaidMember)
+		self:ResizeBars(tRaidMember, bDead)
 		
 		if not tMemberData.bIsOnline or tMemberData.nHealthMax == 0 or tMemberData.nHealth == 0 then
 			nInvalidOrDeadMembers = nInvalidOrDeadMembers + 1
@@ -954,10 +958,10 @@ function BetterRaidFrames:UpdateSpecificMember(tRaidMember, nCodeIdx, tMemberDat
 			wndManaBar:SetMax(nManaMax)
 			wndManaBar:SetProgress(tMemberData.nMana)	
 		end
-		wndManaBar:Show(bShowManaBar and tMemberData.bIsOnline and not bDead and not bOutOfRange and unitMember and unitMember:GetHealth() > 0 and unitMember:GetMaxHealth() > 0)
+		wndManaBar:Show(bShowManaBar and tMemberData.bIsOnline and not bDead and not bOutOfRange)
 		
 		-- Scaling
-		self:ResizeBars(tRaidMember)
+		self:ResizeBars(tRaidMember, bDead)
 	end
 	self:ResizeMemberFrame(wndRaidMember)
 end
@@ -1413,7 +1417,7 @@ function BetterRaidFrames:DoHPAndShieldResizing(tRaidMember, tMemberData)
 	self:SetBarValue(wndCurrHealthBar, 0, nHealthCurr, nHealthMax)
 end
 
-function BetterRaidFrames:ResizeBars(tRaidMember)
+function BetterRaidFrames:ResizeBars(tRaidMember, bDead)
 	local nWidth = tRaidMember.wndRaidMemberBtn:GetWidth() - 4
 	local wndHealthBar = tRaidMember.wndHealthBar
 	local wndMaxAbsorb = tRaidMember.wndMaxAbsorbBar
@@ -1421,8 +1425,8 @@ function BetterRaidFrames:ResizeBars(tRaidMember)
 	local nLeft, nTop, nRight, nBottom = wndHealthBar:GetAnchorOffsets()
 	
 	-- Define offsets based on settings of which bars to show
-	wndMaxShield:Show(self.settings.bShowShieldBar)
-	wndMaxAbsorb:Show(self.settings.bShowAbsorbBar)
+	wndMaxShield:Show(self.settings.bShowShieldBar and not bDead)
+	wndMaxAbsorb:Show(self.settings.bShowAbsorbBar and not bDead)
 	if self.settings.bShowShieldBar and self.settings.bShowAbsorbBar then
 		wndHealthBar:SetAnchorOffsets(nLeft, nTop, nWidth * 0.67, nBottom)
 		wndMaxShield:SetAnchorOffsets(nWidth * 0.67, nTop, nWidth * 0.85, nBottom)