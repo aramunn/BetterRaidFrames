8bf43e864ab40e3af6a478e742cf1d66dd36f20e
NielsH <github@nuvini.com>
2014-08-24 17:00:17 +0200

    Remove StopTimer calls
    Always leave the timer enabled. The handler function will just return if
    not in a raid. This also fixes a bug where in some cases the timer
    wasn't started properly.

--- BetterRaidFrames.lua
+++ BetterRaidFrames.lua
@@ -355,7 +355,6 @@ function BetterRaidFrames:OnDocumentReady()
 	--Apollo.RegisterEventHandler("VarChange_FrameCount", 					"OnRaidFrameBaseTimer", self)
 	Apollo.RegisterTimerHandler("RaidUpdateTimer",							"OnRaidFrameBaseTimer", self)
 	Apollo.CreateTimer("RaidUpdateTimer", 0.2, true)
-	Apollo.StopTimer("RaidUpdateTimer")
 	
 	Apollo.RegisterEventHandler("ChangeWorld", 								"OnChangeWorld", self)
 	Apollo.RegisterTimerHandler("TrackSavedCharactersTimer",				"TrackSavedCharacters", self)
@@ -438,10 +437,6 @@ function BetterRaidFrames:OnDocumentReady()
 	local unitPlayer = GameLib.GetPlayerUnit()
 	if unitPlayer then
 		self:OnCharacterCreated()
-	end
-	
-	if GroupLib.InRaid() then
-		Apollo.StartTimer("RaidUpdateTimer")
 	end
 	
 	-- Refresh settings visually
@@ -593,7 +588,6 @@ function BetterRaidFrames:OnRaidFrameBaseTimer()
 	if not GroupLib.InRaid() then
 		if self.wndMain and self.wndMain:IsValid() and self.wndMain:IsShown() then
 			self.wndMain:Show(false)
-			Apollo.StopTimer("RaidUpdateTimer")
 		end
 		return
 	end
@@ -645,7 +639,6 @@ end
 
 function BetterRaidFrames:OnGroup_Join()
 	if not GroupLib.InRaid() then return end
-	Apollo.StartTimer("RaidUpdateTimer")
 	self.nDirtyFlag = bit32.bor(self.nDirtyFlag, knDirtyGeneral)
 end
 
